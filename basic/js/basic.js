e38041b64913d51de617035bb392ec9de59a02d3
Mo Faraman <mofarman102@gmail.com>
2019-10-22 10:53:36 +0200

    names works now

--- basic/js/basic.js
+++ basic/js/basic.js
@@ -1,17 +1,23 @@
-var i = 0;
 
-for (i = 0; i < 9; i++) {
 
-    $(document).ready(function () {
 
-        rand = Math.floor(Math.random() * 3);
-        console.log(rand);
-        $(".container").append(`
-<div class="box" value="${christmas[rand].name}"><img src="${christmas[rand].img}" alt=""></div>`
-        );
-$("box").on(`click`,function(){
-    $( ".box" ).click( handler ).find( valueof(".box") ) ;
 
-})
-    })
-};
+      
+        var i = 0;
+
+        for (i = 0; i < 9; i++) {
+
+            rand = Math.floor(Math.random() * 3);
+            console.log(rand);
+            $(".container").append(`
+    <div class="box" value=${christmas[rand].name}><img src=${christmas[rand].img} alt=""></div>`
+            );
+    
+        };
+        $(".box").click(function(){
+            var v=$(this).attr("value");
+            console.log(v);
+        
+        })
+
+
