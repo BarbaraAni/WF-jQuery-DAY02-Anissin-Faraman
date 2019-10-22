b9f9c65a4039011ad509daf988c415758c40d6cc
Barbara Anissin <anissinb@gmail.com>
2019-10-22 11:09:10 +0200

    correct js

--- basic/js/basic.js
+++ basic/js/basic.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 var i = 0;
->>>>>>> 62bfac47f98650ba8c6a48a865ca3af9738ad313
 
 for (i = 0; i < 9; i++) {
 
@@ -13,33 +10,10 @@ for (i = 0; i < 9; i++) {
 };
 $(".box").click(function() {
     var v = $(this).attr("value");
-    console.log(v);
+    if (v == "Santa") {
+        $(this).remove();
+    } else {
+        $("#output").text(v);
+    }
 
-<<<<<<< HEAD
-      
-        var i = 0;
-
-        for (i = 0; i < 9; i++) {
-
-            rand = Math.floor(Math.random() * 3);
-            console.log(rand);
-            $(".container").append(`
-    <div class="box" value=${christmas[rand].name}><img src=${christmas[rand].img} alt=""></div>`
-            );
-    
-        };
-        $(".box").click(function(){
-            var v=$(this).attr("value");
-        if(v=="Santa"){
-            $(this).remove();
-        }   
-        else{
-            $("#output").text(v);
-        }
-        
-        })
-
-
-=======
 })
->>>>>>> 62bfac47f98650ba8c6a48a865ca3af9738ad313
